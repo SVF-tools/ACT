e8eb29a22dc1ebd973eb2ebd38461cd5997716d6
BUPTlkj <xxxy_lkj@163.com>
2026-01-27 15:41:22 +1100

    update

--- act/util/path_config.py
+++ act/util/path_config.py
@@ -128,6 +128,11 @@ def get_modules_root() -> str:
     return os.path.join(get_project_root(), 'modules')
 
 
+def get_examples_nets_dir() -> str:
+    """Get the examples nets directory path."""
+    return os.path.join(get_project_root(), 'act', 'back_end', 'examples', 'nets')
+
+
 def get_eran_tf_verify_path() -> str:
     """Get the ERAN tf_verify directory path."""
     return os.path.join(get_modules_root(), 'eran', 'tf_verify')
